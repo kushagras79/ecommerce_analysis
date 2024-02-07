d0b178e63609b7639b06bf08f029ae08d664cf39
Kushagra <kushagras79@gmail.com>
2024-02-07 10:09:22 +0530

    Removing obslete code

--- lib/dataReader.py
+++ lib/dataReader.py
@@ -10,13 +10,6 @@ def get_customers_schema():
         StructField('customer_state',StringType())
     ])
     return cust_schema
-def read_customers():
-    spark = getSparkSession()
-    cust_df = spark.read.format('csv').\
-    schema(get_customers_schema()).\
-    option('header','true').\
-    load('data/raw/olist_customers_dataset.csv')
-    return cust_df
     
 def get_geolocation_schema():
     geo_schema = StructType([
@@ -27,13 +20,6 @@ def get_geolocation_schema():
         StructField('geolocation_state',StringType())
     ])
     return geo_schema
-def read_geoloc():
-    spark = getSparkSession()
-    geoLoc = spark.read.format('csv').\
-    schema(get_geolocation_schema()).\
-    option('header','true').\
-    load('data/raw/olist_geolocation_dataset.csv')
-    return geoLoc
 
 def get_seller_schema():
     seller_schema = StructType([
@@ -43,13 +29,6 @@ def get_seller_schema():
         StructField('seller_state',StringType())
     ])
     return seller_schema
-
-def read_seller():
-    spark = getSparkSession()
-    return spark.read.format('csv').\
-    schema(get_seller_schema()).\
-    option('header','true').\
-    load('data/raw/olist_sellers_dataset.csv')
 
 def get_orders_schema():
     orders_schema = StructType([
@@ -63,12 +42,6 @@ def get_orders_schema():
         StructField('order_estimated_delivery_date',StringType())
         ])
     return orders_schema
-def read_orders():
-    spark = getSparkSession()
-    orders_df = spark.read.format('csv').schema(get_orders_schema()).option('mode','permissive').\
-        option('header','true').option('inferSchema','false').\
-        load('data/raw/olist_orders_dataset.csv')
-    return orders_df
 
 def get_orders_items_schema():
     orders_items_schema = StructType([
@@ -82,31 +55,17 @@ def get_orders_items_schema():
     ])
     return orders_items_schema
 
-def read_orders_item():
-    spark = getSparkSession()
-    return spark.read.format('csv').schema(get_orders_items_schema()).\
-    option('header','true').\
-    option('inferSchema','false').\
-    load('data/raw/olist_order_items_dataset.csv')
 
 def get_products_schema():
     product_schema = 'product_id string,product_category_name string,product_name_lenght integer,product_description_lenght integer,product_photos_qty	integer, product_weight_g integer,	product_length_cm integer,product_height_cm integer,product_width_cm integer'
     return product_schema
 
-def read_products():
-    spark = getSparkSession()
-    return spark.read.format('csv').schema(get_products_schema()).option('header','true').load('data/raw/olist_products_dataset.csv')
 
 def get_products_category_schema():
     return StructType([
             StructField('product_category_name',StringType()),
             StructField('product_category_name_english',StringType())
     ])
-
-def read_products_category():
-    spark = getSparkSession()
-    return spark.read.format('csv').schema(get_products_category_schema()).load('data/raw/product_category_name_translation.csv')
-
 
 def create_df(mySchema,filePath):
     spark = getSparkSession()
